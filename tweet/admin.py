585600ea3a7c4612b6b39e0537356e4d328754ce
navjyotsingh17 <navjyotpulaha@gmail.com>
2024-10-01 16:01:21 +0530

    twitter app using django

---
+++ tweet/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Tweet
+from .models import UserProfile
+
+# Register your models here.
+
+# admin.site.register(Tweet)
+admin.site.register(UserProfile)
